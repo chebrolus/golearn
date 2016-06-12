393672317d78d06bb1d8893e54f68b5bffebedfd
Srini Chebrolu <schebrolu@apigee.com>
2016-06-12 12:52:00 -0700

    data input array and random selection for benchmark tests; added comments

--- stringutil/palindrome_test.go
+++ stringutil/palindrome_test.go
@@ -1,8 +1,12 @@
 package stringutil
 
-import "testing"
+import (
+	"testing"
+	"math/rand"
+)
 
 func TestIsPalindrome(t *testing.T) {
+	// test data with expected result
 	var tests = []struct {
 		s            string
 		isPalindrome bool
@@ -18,16 +22,22 @@ func TestIsPalindrome(t *testing.T) {
 		{"toyota", false},
 		{"not a palindrome", false},
 	}
+
+	// for each entry in test data apply function and compare with expected result
 	for _, c := range tests {
 		got := isPalindrome(c.s)
 		if got != c.isPalindrome {
+			// fail if the result doesn't match expected
 			t.Errorf("isPalindrome(%v) == %v, want %v", c.s, got, c.isPalindrome)
 		}
 	}
 }
 
 func BenchmarkIsPalindrome(b *testing.B) {
+	// input array
+	ip := []string{"", "civic", "a", "aloha", "ahoha", "palindrome", "verylongstringforpalindrometest", "aaaaabbbbcccddeddcccbbbbaaaaa"}
 	for i := 1; i < b.N; i++ {
-		isPalindrome("ReverseeveR")
+		// random input selection from the given input array
+		isPalindrome(ip[rand.Intn(len(ip))])
 	}
 }
--- stringutil/reverse_test.go
+++ stringutil/reverse_test.go
@@ -1,8 +1,12 @@
 package stringutil
 
-import "testing"
+import (
+	"testing"
+	"math/rand"
+)
 
 func TestReverse(t *testing.T) {
+	// test data with expected result
 	var tests = []struct {
 		s, want string
 	}{
@@ -13,13 +17,17 @@ func TestReverse(t *testing.T) {
 	for _, c := range tests {
 		got := Reverse(c.s)
 		if got != c.want {
+			// fail if function response doesn't match with expected result
 			t.Errorf("Reverse(%v) == %v, want %v", c.s, got, c.want)
 		}
 	}
 }
 
 func BenchmarkReverse(b *testing.B) {
+	// input array
+	ip := []string{"", "a", "ReverseTest", "verylongstringforreversetest", "verylongstringforreversetestverylongstringforreversetestverylongstringforreversetest"}
 	for i := 1; i < b.N; i++ {
-		Reverse("ReverseTest")
+		// random input selection from the given input array
+		Reverse(ip[rand.Intn(len(ip))])
 	}
 }
